5137d1342fbafe31c1e7e50549532381f757bd24
satyamgupta09 <138917027+satyamgupta09@users.noreply.github.com>
2025-01-29 03:35:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,17 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
+import Flames from "./Flames";
 import '../styles/App.css';
+// import "./styles.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <h1>Flames App</h1>
+        <Flames />
+      </div>
+    );
+  }
 }
-
 
 export default App;
